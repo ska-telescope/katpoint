8f31179be1ef90323d00438c6ee2bfe268926261
David Terrett <david.terrett@stfc.ac.uk>
2020-01-31 09:30:18 +0000

    Update Moon position for astropy 4.0

--- katpoint/test/test_body.py
+++ katpoint/test/test_body.py
@@ -68,8 +68,8 @@ class TestFixedBody(unittest.TestCase):
         body.compute(obs)
 
         # 127:15:23.6 60:05:13.7'
-        self.assertEqual(body.az.to_string(sep=':'), '127:15:46.3993')
-        self.assertEqual(body.alt.to_string(sep=':'), '60:05:18.6244')
+        self.assertEqual(body.az.to_string(sep=':'), '127:15:17.1374')
+        self.assertEqual(body.alt.to_string(sep=':'), '60:05:10.2433')
 
     def test_sun(self):
         obs = Observer()
