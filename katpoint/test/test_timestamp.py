2f3e8e53a70d643384010fdf3dc472647890ebae
Ludwig Schwardt <ludwig@ska.ac.za>
2020-07-14 01:23:11 +0200

    Use pytest parametrizations
    Break the test class apart into individual functions. Fix string
    formatting. Remove the disabled timestamp overflow tests that
    highlighted an issue on macOS back in 2009 (see bef49f4).

--- katpoint/test/test_timestamp.py
+++ katpoint/test/test_timestamp.py
@@ -22,67 +22,67 @@ from astropy.time import Time
 import katpoint
 
 
-class TestTimestamp:
-    """Test timestamp creation and conversion."""
+@pytest.mark.parametrize(
+    'init_value, string',
+    [
+        (1248186982.3980861, '2009-07-21 14:36:22.398'),
+        (Time('2009-07-21 02:52:12.34'), '2009-07-21 02:52:12.340'),
+        (0, '1970-01-01 00:00:00'),
+        (-10, '1969-12-31 23:59:50'),
+        ('2009-07-21 02:52:12.034', '2009-07-21 02:52:12.034'),
+        ('2009-07-21 02:52:12.000', '2009-07-21 02:52:12'),
+        ('2009-07-21 02:52:12', '2009-07-21 02:52:12'),
+        ('2009-07-21 02:52', '2009-07-21 02:52:00'),
+        ('2009-07-21', '2009-07-21 00:00:00'),
+        ('2009/07/21 02:52:12.034', '2009-07-21 02:52:12.034'),
+        ('2009/07/21 02:52:12.000', '2009-07-21 02:52:12'),
+        ('2009/07/21 02:52:12', '2009-07-21 02:52:12'),
+        ('2009/07/21 02:52', '2009-07-21 02:52:00'),
+        ('2009/07/21', '2009-07-21 00:00:00'),
+        ('2019-07-21 02:52:12', '2019-07-21 02:52:12')
+    ]
+)
+def test_construct_valid_timestamp(init_value, string):
+    t = katpoint.Timestamp(init_value)
+    assert str(t) == string, (
+        "Timestamp string ('{}') differs from expected one ('{}')".format(str(t), string))
 
-    def setup(self):
-        self.valid_timestamps = [(1248186982.3980861, '2009-07-21 14:36:22.398'),
-                                 (Time('2009-07-21 02:52:12.34'), '2009-07-21 02:52:12.340'),
-                                 (0, '1970-01-01 00:00:00'),
-                                 (-10, '1969-12-31 23:59:50'),
-                                 ('2009-07-21 02:52:12.034', '2009-07-21 02:52:12.034'),
-                                 ('2009-07-21 02:52:12.000', '2009-07-21 02:52:12'),
-                                 ('2009-07-21 02:52:12', '2009-07-21 02:52:12'),
-                                 ('2009-07-21 02:52', '2009-07-21 02:52:00'),
-                                 ('2009-07-21', '2009-07-21 00:00:00'),
-                                 ('2009/07/21 02:52:12.034', '2009-07-21 02:52:12.034'),
-                                 ('2009/07/21 02:52:12.000', '2009-07-21 02:52:12'),
-                                 ('2009/07/21 02:52:12', '2009-07-21 02:52:12'),
-                                 ('2009/07/21 02:52', '2009-07-21 02:52:00'),
-                                 ('2009/07/21', '2009-07-21 00:00:00'),
-                                 ('2019-07-21 02:52:12', '2019-07-21 02:52:12')]
-        self.invalid_timestamps = ['gielie', '03 Mar 2003']
-        self.overflow_timestamps = ['2049-07-21 02:52:12']
 
-    def test_construct_timestamp(self):
-        """Test construction of timestamps."""
-        for v, s in self.valid_timestamps:
-            t = katpoint.Timestamp(v)
-            assert str(t) == s, (
-                "Timestamp string ('%s') differs from expected one ('%s')"
-                % (str(t), s))
-        for v in self.invalid_timestamps:
-            with pytest.raises(ValueError):
-                katpoint.Timestamp(v)
-#        for v in self.overflow_timestamps:
-#            with pytest.raises(OverflowError):
-#               katpoint.Timestamp(v)
+@pytest.mark.parametrize('init_value', ['gielie', '03 Mar 2003'])
+def test_construct_invalid_timestamp(init_value):
+    with pytest.raises(ValueError):
+        katpoint.Timestamp(init_value)
 
-    def test_numerical_timestamp(self):
-        """Test numerical properties of timestamps."""
-        t = katpoint.Timestamp(self.valid_timestamps[0][0])
-        assert t == t + 0.0
-        assert t != t + 1.0
-        assert t > t - 1.0
-        assert t < t + 1.0
-        assert t == eval('katpoint.' + repr(t))
-        assert float(t) == self.valid_timestamps[0][0]
-        t = katpoint.Timestamp(self.valid_timestamps[1][0])
-        assert t.time == self.valid_timestamps[1][0]
-        try:
-            assert hash(t) == hash(t + 0.0), 'Timestamp hashes not equal'
-        except TypeError:
-            pytest.fail('Timestamp object not hashable')
 
-    def test_operators(self):
-        """Test operators defined for timestamps."""
-        T = katpoint.Timestamp(self.valid_timestamps[0][0])
-        S = T.secs
-        # Logical operators, float treated as absolute time
-        assert T == S
-        assert T < S + 1
-        assert T > S - 1
-        # Arithmetic operators, float treated as interval
-        assert isinstance(T - S, katpoint.Timestamp)
-        assert isinstance(S - T, float)
-        assert isinstance(T - T, float)
+def test_numerical_timestamp():
+    """Test numerical properties of timestamps."""
+    t0 = 1248186982.3980861
+    t = katpoint.Timestamp(t0)
+    assert t == t + 0.0
+    assert t != t + 1.0
+    assert t > t - 1.0
+    assert t < t + 1.0
+    assert t == eval('katpoint.' + repr(t))
+    assert float(t) == t0
+    t1 = Time('2009-07-21 02:52:12.34')
+    t = katpoint.Timestamp(t1)
+    assert t.time == t1
+    try:
+        assert hash(t) == hash(t + 0.0), 'Timestamp hashes not equal'
+    except TypeError:
+        pytest.fail('Timestamp object not hashable')
+
+
+def test_operators():
+    """Test operators defined for timestamps."""
+    t0 = 1248186982.3980861
+    t = katpoint.Timestamp(t0)
+    s = t.secs
+    # Logical operators, float treated as absolute time
+    assert t == s
+    assert t < s + 1
+    assert t > s - 1
+    # Arithmetic operators, float treated as interval
+    assert isinstance(t - s, katpoint.Timestamp)
+    assert isinstance(s - t, float)
+    assert isinstance(t - t, float)
