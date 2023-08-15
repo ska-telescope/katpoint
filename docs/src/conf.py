6c764dd60cc19fd7c4ab041cf6dd613681338417
Ludwig Schwardt <ludwig@ska.ac.za>
2023-08-15 16:29:52 +0200

    SPAZA-299 Fix docs config
    Since the docs now live in a src/ of their own, go one directory up
    to find the package source.
    
    Update the copyright year while we are at it.

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -2,7 +2,7 @@
 #
 # This file only contains a selection of the most common options. For a full
 # list see the documentation:
-# http://www.sphinx-doc.org/en/master/config
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 # -- Path setup --------------------------------------------------------------
 
@@ -12,20 +12,23 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../src'))
+sys.path.insert(0, os.path.abspath('../../src'))
 import katpoint
 
 
 # -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'katpoint'
-copyright = '2019, South African Radio Astronomy Observatory'
+copyright = '2009-2023, South African Radio Astronomy Observatory'
 author = 'South African Radio Astronomy Observatory'
 
 version = '.'.join(katpoint.__version__.split('.')[:2])
 release = katpoint.__version__
 
+
 # -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
@@ -47,6 +50,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 # -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
