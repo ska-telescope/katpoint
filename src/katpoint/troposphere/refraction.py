SPAZA-300 Fix refraction weather unit conversion
I had mistakenly assumed that `u.quantity_input` would also convert
incoming parameters to the canonical units via `.to()`. Instead, it
only checks compatibility and leaves conversion to the user. It is
unsafe to just use parameter `.value` instead of `.to_value(unit)`.

This was uncovered by mixing up the weather units in the closure unit
tests (hPa -> kPa and deg_C -> K). Keep those tests with tweaked units.

There is no need to form zenith angles explicitly if you are only
interested in tan(z) = 1 / tan(el).

Also do some minor docstring improvements.

src/katpoint/troposphere/refraction.py
@@ -73,7 +73,7 @@ class _RefractionModel:
         temperature: u.deg_C,
         relative_humidity: u.dimensionless_unscaled,
     ) -> u.deg:
-        """Calculate unrefracted elevation angle from `refracted_el`.
+        """Calculate unrefracted elevation angle from `refracted_elevation`.
 
         This undoes a refraction correction that resulted in the given elevation
         angle. It is the inverse of :meth:`refract`.
@@ -139,8 +139,9 @@ class HaystackRefraction(_RefractionModel):
         relative_humidity: u.dimensionless_unscaled,
     ) -> u.deg:
         elevation = Angle(elevation)
-        pressure_hPa = pressure.value
-        temperature_C = temperature.value
+        pressure_hPa = pressure.to_value(u.hPa)
+        temperature_C = temperature.to_value(u.deg_C)
+        humidity = u.Quantity(relative_humidity).to_value(u.dimensionless_unscaled)
 
         p = (0.458675e1, 0.322009e0, 0.103452e-1, 0.274777e-3, 0.157115e-5)
         cvt = 1.33289
@@ -154,7 +155,7 @@ class HaystackRefraction(_RefractionModel):
 
         # Compute SN (surface refractivity)
         # (via dewpoint and water vapor partial pressure? [LS])
-        rhumi = 100.0 * (1.0 - relative_humidity) * 0.9
+        rhumi = 100.0 * (1.0 - humidity) * 0.9
         dewpt = temperature_C - rhumi * (
             0.136667 + rhumi * 1.33333e-3 + temperature_C * 1.5e-3
         )
@@ -175,8 +176,8 @@ class HaystackRefraction(_RefractionModel):
         el = np.clip(elevation, 1.0 * u.deg, 90.0 * u.deg)
         aphi = a / ((el.deg + b) ** c)
         dele = -d / ((el.deg + e) ** f)
-        zenith_angle = 90.0 * u.deg - el
-        bphi = g * (np.tan(zenith_angle) + dele)
+        tan_z = 1.0 / np.tan(el)
+        bphi = g * (tan_z + dele)
         # Threw out an (el < 0.01) check here,
         # which will never succeed because el is clipped to be above 1.0 [LS]
 
@@ -313,7 +314,7 @@ class TroposphericRefraction:
     ) -> AltAz:
         """Remove refraction correction from (az, el) coordinates.
 
-        Given `refracted_azel`, the apparent direction of incoming
+        Given `refracted_azel`, the observed direction of incoming
         electromagnetic waves in the presence of the Earth's atmosphere,
         produce `azel`, the direction of the radiation in the absence of
         the atmosphere. This therefore transforms surface coordinates to

tests/test_troposphere.py
@@ -40,8 +40,6 @@ except ImportError:
 else:
     HAS_ALMACALC = True
 
-from .helper import assert_angles_almost_equal
-
 
 def test_refraction_basic():
     """Test basic refraction correction properties."""
@@ -76,6 +74,7 @@ def test_refraction_haystack(el, pressure, temp, humidity, refracted_el):
 
 def test_refraction_closure():
     """Test closure between refraction correction and its reverse operation."""
+    atol = 0.03 * u.arcsec
     tropo = katpoint.TroposphericRefraction()
     el = Angle(np.arange(0.0, 90.1, 0.1), u.deg)
     azel = AltAz(az=Angle(np.zeros_like(el)), alt=el)
@@ -86,30 +85,24 @@ def test_refraction_closure():
     # Test closure on el grid
     refracted_azel = tropo.refract(azel, pressure, temp, humidity)
     reversed_azel = tropo.unrefract(refracted_azel, pressure, temp, humidity)
-    np.testing.assert_allclose(
-        reversed_azel.alt,
-        el,
-        rtol=0.0,
-        atol=0.03 * u.arcsec,
-        err_msg="Elevation closure error for "
-        f"pressure={pressure:g}, temp={temp:g}, humidity={humidity:g}",
+    msg = (
+        "Elevation closure / consistency error for "
+        f"pressure={pressure:g}, temp={temp:g}, humidity={humidity:g}"
     )
-    # Generate random meteorological data,
-    # now one weather measurement per elevation value.
-    pressure = (900.0 + 200.0 * np.random.rand(len(el))) * u.hPa
-    temp = (-10.0 + 50.0 * np.random.rand(len(el))) * u.deg_C
+    np.testing.assert_allclose(reversed_azel.alt, el, rtol=0.0, atol=atol, err_msg=msg)
+    # Generate random meteorological data, one weather sample per elevation value.
+    # Also test out different units for the parameters.
+    pressure = (90.0 + 20.0 * np.random.rand(len(el))) * u.kPa
+    temp = (273.15 - 10.0 + 50.0 * np.random.rand(len(el))) * u.K
     humidity = (5.0 + 90.0 * np.random.rand(len(el))) * u.percent
     # Test closure on el grid
     refracted_azel = tropo.refract(azel, pressure, temp, humidity)
     reversed_azel = tropo.unrefract(refracted_azel, pressure, temp, humidity)
-    np.testing.assert_allclose(
-        reversed_azel.alt,
-        el,
-        rtol=0.0,
-        atol=0.03 * u.arcsec,
-        err_msg="Elevation closure error for "
-        f"pressure={pressure}, temp={temp}, humidity={humidity}",
+    msg = (
+        "Elevation closure / consistency error for "
+        f"pressure={pressure}, temp={temp}, humidity={humidity}"
     )
+    np.testing.assert_allclose(reversed_azel.alt, el, rtol=0.0, atol=atol, err_msg=msg)
 
 
 def test_delay_basic():